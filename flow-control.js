c4f8d4094dd595c6e04511a972e97bf8157521f5
Pratik <pr.tikgohil@gmail.com>
2020-02-21 17:32:59 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,7 +18,7 @@ function ageChecker(age) {
   if (age > 20) {
     return "You are a grownup"
   } else if (age >= 13 && age < 19) {
-    return "You are a teenager"
+    return "You are a teenager!"
   } else if (age < 13) {
     return "You are a kid"
   }
